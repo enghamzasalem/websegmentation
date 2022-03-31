4ed69b0ad64c6ed09b98b6635065fd4c3538f5a9
Hamza Salem <enghamzasalem77@gmail.com>
2022-03-31 22:36:26 +0000

    push new links

--- main.py
+++ main.py
@@ -26,13 +26,65 @@ for l in next_page_node:
     # print(len(soup.find("a")))
     response = requests.get(url, headers=headers).text
     soup = BeautifulSoup(response, 'lxml')
-    start=url.find("http://") if url.find("http://")!=-1 else url.find("https://")+1
-    t = url[start+11:url.find(" ",start+11)]
-    f = open("newHTML1/"+str(len(allarr))+".html", "x")
-    ff = open("newHTMLfull1/"+str(len(allarr))+".html", "x")
-    # f = open(str(url)+".txt", "x")
-    f.write(str(soup.find("script",{"type":"application/ld+json"})))
-    ff.write(str(soup))
-    f.close()
-    ff.close()
+    h1=soup.find("h1").text
+    print(h1)
+    articledivs=soup.find("article")
+    
+    divs=soup.find_all("div")
+    i=0
+    maxx=1
+    alldivs=[]
+    print(len(divs))
+    for i in divs:
+      # print(alldivs)
+      # alldivs.append(divs[i].text)
+      l=int(len(i))
+      if l>maxx:
+        body=i.text
+        maxx=len(i.text)
+        
+      #   # print(max)
+        # alldivs.append(divs[i].text)
+    # print(alldivs)
+    # print(alldivs)
+    # hi = max(alldivs) 
+    # max = max(divs, key=len)
+    # max_length,longest_element = max([(len(x),x) for x in alldivs ])
+    print(maxx)
+    print(body)
+    # start=url.find("http://") if url.find("http://")!=-1 else url.find("https://")+1
+    # t = url[start+11:url.find(" ",start+11)]
+    fn = open("newHTMLinjected3/"+str(len(allarr))+".json", "x")
+    if articledivs is not None:
+      token=url.split('https://')[1].split('/')[0]
+      top_level=token.split('.')[-2]+'.'+token.split('.')[-1]
+      jsonID={
+                    "@context": "https://schema.org",
+                    "@type": "WebPage",
+                    "name": h1,
+                    "description":  articledivs.text,
+                    "publisher": {
+                        "@type": "Organization",
+                        "name": soup.title.get_text().split("|")[-1],
+                        "url": top_level,
+                        "alternateName": soup.title.get_text().split("|")[-1],
+                        "logo": {
+                            "@type": "ImageObject",
+                            "url": "https://www.skynewsarabia.com/icon.png",
+                            "width": 192,
+                            "height": 192
+                        }
+                    },
+                    "identifier": url,
+                    "url":url
+                }
+      fn.write(str(jsonID))
+      fn.close()
+    # f = open("newHTML1/"+str(len(allarr))+".html", "x")
+    # ff = open("newHTMLfull1/"+str(len(allarr))+".html", "x")
+    # # f = open(str(url)+".txt", "x")
+    # f.write(str(soup.find("script",{"type":"application/ld+json"})))
+    # ff.write(str(soup))
+    # f.close()
+    # ff.close()
 print(allarr)
